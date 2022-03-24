be7c87ad426bcb6383087eebb1cb64122d200804
Golam Mursalin <golammursalin309@gmail.com>
2022-03-25 02:06:36 +0600

    card meals added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 
 // Components
 import Header from "./components/Layout/Header";
-
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
